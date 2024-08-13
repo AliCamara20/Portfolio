5177b7102c6ac3a37245ec3b73cbebeed4c05112
Alicamara20 <ali.camara1955@gmail.com>
2024-08-13 03:15:42 +0100

    my second portfolio commit

--- Porfolio.js
+++ Porfolio.js
@@ -1,7 +1,12 @@
 const hamMenu = document.querySelector(".hamMenu");
 const offScreenMenu = document.querySelector(".offScreenMenu");
 const form = document.forms["form"];
+const errorMessage = document.getElementById("error");
+
 const errors = [];
+
+
+//inputs.forEach( input => input.addEventListener("input", document.getElementById("error").innerHTML = ""));
 
 
 
@@ -18,18 +23,38 @@ function hide(){
 }        
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let name = document.getElementById("name").value;
+        e.preventDefault();
+    let fullName = document.getElementById("name").value;
     let email = document.getElementById("email").value;
-    if(name.length <= 0){
-        document.getElementById.innerHTML = 'Name fill is empty, please enter your name it is required';
+
+    if(fullName.length <= 0){
+        errors.push("Name fill is empty, please enter your name it is required.<br>")
+        //document.getElementById("error").innerHTML = 'Name fill is empty, please enter your name it is required';
     }
 
     if(email.length <= 0){
-        document.getElementById("email").innerHTML = 'Email fill is empty, please enter your name it is required';
+        errors.push("Email fill is empty, please enter your name it is required.<br>")
+        //document.getElementById("error").innerHTML = 'Email fill is empty, please enter your name it is required';
     }
+
+    else{
+        errors.push("form submitted successfully");
+        errorMessage.style.color = "green";
+        
+    }
+
+    errorMessage.innerHTML = errors.join("") ;
+
 
 
 
 
 });
+
+const inputs = [fullName, email];
+
+Array.from(inputs).forEach( input => input.addEventListener("input", () => {document.getElementById("error").innerHTML = ""}));
+
+function fade(){
+    errorMessage.innerHTML = ""
+}
